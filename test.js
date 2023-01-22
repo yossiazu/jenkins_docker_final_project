1908d041d70567d11fca810ff77905d0aecfd381
yossiazu <121163420+yossiazu@users.noreply.github.com>
2023-01-22 21:47:13 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-echo "hello test.js"
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is the file test.js</h1>
+<p>hello from java script.</p>
+
+</body>
+</html>
